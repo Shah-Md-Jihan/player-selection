a41031b62c127c3d33c9c20707b589edaae07bf0
Shah-Md-Jihan <shahmohammadjihan@gmail.com>
2022-08-23 00:23:30 +0600

    player selection functionality and validation added

---
+++ js/app.js
@@ -0,0 +1,18 @@
+document.getElementById('player1_btn').addEventListener('click', function () {
+    playerSelection('max-well', 'player1_btn');
+});
+document.getElementById('player2_btn').addEventListener('click', function () {
+    playerSelection('villers', 'player2_btn');
+});
+document.getElementById('player3_btn').addEventListener('click', function () {
+    playerSelection('gayle', 'player3_btn');
+});
+document.getElementById('player4_btn').addEventListener('click', function () {
+    playerSelection('boult', 'player4_btn');
+});
+document.getElementById('player5_btn').addEventListener('click', function () {
+    playerSelection('zampa', 'player5_btn');
+});
+document.getElementById('player6_btn').addEventListener('click', function () {
+    playerSelection('anderson', 'player6_btn');
+});
---
+++ js/core.js
@@ -0,0 +1,21 @@
+function playerSelection(playerId, buttonId) {
+    const out = document.getElementById('PlayerNameContainer').childNodes.length;
+    // no selection alert 
+    const noSelectionAlert = document.getElementById('NoneAlert');
+    noSelectionAlert.innerText = '';
+
+    // selection validation, button dissabled option 
+    if (out < 6) {
+        const player_name = document.getElementById(playerId).innerText;
+        const li = document.createElement('li');
+
+        li.innerText = player_name;
+        PlayerNameContainer.appendChild(li);
+        document.getElementById(buttonId).setAttribute('disabled', true);
+        return;
+    } else {
+        alert("you can't hire more than 5 foreign players!");
+    }
+
+}
+
